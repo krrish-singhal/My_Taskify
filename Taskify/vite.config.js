447d0935b451ff30a4223a04e9f1a821b0921f53
krrish <krrishsinghal62@gmail.com>
2025-07-20 16:23:47 +0530

    Fix define object in vite.config.js for Vercel deployment

--- Taskify/vite.config.js
+++ Taskify/vite.config.js
@@ -1,17 +1,27 @@
-import { defineConfig, loadEnv } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig, loadEnv } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig(({ mode }) => {
-  // Load environment variables for the correct mode
-  const env = loadEnv(mode, process.cwd(), '');
+  const env = loadEnv(mode, process.cwd(), '')
 
   return {
     plugins: [react()],
     define: {
-      'process.env': env,
+      "process.env.VITE_API_URL": JSON.stringify(process.env.VITE_API_URL),
+      "process.env.VITE_CLIENT_URL": JSON.stringify(
+        process.env.VITE_CLIENT_URL
+      ),
     },
+
     server: {
       port: 5173,
+      proxy: {
+        "/api": {
+          target: "http://localhost:5000",
+          changeOrigin: true,
+          secure: false,
+        },
+      },
     },
   };
-});
+})
